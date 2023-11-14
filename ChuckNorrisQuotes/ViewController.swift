59f838db7274f521737eae3556b30adbae7ffef8
Teo <teccent@yandex.ru>
2023-11-14 20:40:01 +0300

    Initial Commit

---
+++ ChuckNorrisQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorrisQuotes
+//
+//  Created by Теона Магай on 14.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
